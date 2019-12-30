a6db4f032cf59fc905c205dd1da170f990b08e91
Jason Dent <jason@streetsidesoftware.nl>
2019-12-30 13:42:39 +0100

    Export operators

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './GenSequence';
 export { Sequence, LazyIterable, IterableLike, SequenceBuilder } from './util/types';
 export * from './builder';
+
+import * as _operators from './operators';
+export const operators = _operators;
